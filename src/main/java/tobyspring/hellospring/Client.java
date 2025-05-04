3527e60ec7f17ef1802dc5385a808d0b1c600805
LEECHANHEE <chanhee1158@gmail>
2025-05-04 20:11:26 +0900

    <Refactor>: 관계설정 책임의 분리

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -7,7 +7,7 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
 
-        PaymentService paymentService = new PaymentService();
+        PaymentService paymentService = new PaymentService(new WebApiExPrateProvider());
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
