33d4e3d79e348234393a445ae1060f24c4df0d0b
LEECHANHEE <chanhee1158@gmail>
2025-06-01 16:53:49 +0900

    <Feat>: 프록시 패턴을 이용한 트랜잭션 관심사 분리

--- src/main/java/tobyspring/hellospring/OrderConfig.java
+++ src/main/java/tobyspring/hellospring/OrderConfig.java
@@ -3,12 +3,12 @@ package tobyspring.hellospring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import tobyspring.hellospring.data.JdbcOrderRepository;
-import tobyspring.hellospring.data.JpaOrderRepository;
 import tobyspring.hellospring.order.OrderRepository;
 import tobyspring.hellospring.order.OrderService;
+import tobyspring.hellospring.order.OrderServiceImpl;
+import tobyspring.hellospring.order.OrderServiceTxProxy;
 
 import javax.sql.DataSource;
 
@@ -17,7 +17,7 @@ import javax.sql.DataSource;
 public class OrderConfig {
     @Bean
     public OrderService orderService(OrderRepository orderRepository, PlatformTransactionManager platformTransactionManager) {
-        return new OrderService(orderRepository, platformTransactionManager);
+        return new OrderServiceTxProxy(new OrderServiceImpl(orderRepository), platformTransactionManager);
     }
 
     @Bean
